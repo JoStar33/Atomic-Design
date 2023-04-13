e9c707fbac7e0a6dcadec9bd54198f04f4015463
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-13 10:25:45 +0900

    chore: input ui 수정

--- components/atomic/Input/Input.tsx
+++ components/atomic/Input/Input.tsx
@@ -1,9 +1,10 @@
 interface Props {
   label: string;
+  placeholder: string;
   setValue: (value: string) => {};
 }
 
-const Input = ({ label, setValue }: Props) => {
+const Input = ({ label, placeholder, setValue }: Props) => {
   const handleClick = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   }
@@ -19,10 +20,8 @@ const Input = ({ label, setValue }: Props) => {
         <input
           onChange={handleClick}
           type="text"
-          name="last-name"
-          id="last-name"
-          autoComplete="family-name"
-          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-500 sm:text-sm sm:leading-6"
+          placeholder={placeholder}
+          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-offset-0 focus:ring-purple-500 sm:text-sm sm:leading-6"
         />
       </div>
     </div>
