222f56fa85b54a1cca602227cab3ba8e10788365
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-26 16:23:29 +0900

    feat: 다이얼로그 전역 설정 완료

--- pages/_app.app.tsx
+++ pages/_app.app.tsx
@@ -1,6 +1,20 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.scss'
+import ValidateDialog from '@/components/organisms/dialogs/ValidateDialog/ValidateDialog';
+import { DialogContext } from '@/utils/DialogContext';
+import type { AppProps } from 'next/app';
+import { useRef, useState } from 'react';
+import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const dialogText = useRef<string>('');
+  const [dialogShow, setDialogShow] = useState<boolean>(false);
+  const setDialog = (text: string, isShow: boolean) => {
+    setDialogShow(isShow);
+    dialogText.current = text;
+  }
+  return (
+    <DialogContext.Provider value={{setDialog}}>
+      <ValidateDialog dialogShow={dialogShow} dialogText={dialogText.current} handleDialog={() => setDialogShow(false)}/>
+      <Component {...pageProps} />
+    </DialogContext.Provider>
+  );
 }
