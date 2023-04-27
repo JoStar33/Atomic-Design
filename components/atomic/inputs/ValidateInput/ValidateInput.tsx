154c5db3ada69cb902f66553c79ebeba9a498376
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-27 18:17:39 +0900

    feat: 유효성검사 인풋 1차 구현

---
+++ components/atomic/inputs/ValidateInput/ValidateInput.tsx
@@ -0,0 +1,14 @@
+
+const ValidateInput = () => {
+  return (
+    <div>
+      <label htmlFor=""></label>
+      <div>
+        <input type="text" />
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default ValidateInput;
