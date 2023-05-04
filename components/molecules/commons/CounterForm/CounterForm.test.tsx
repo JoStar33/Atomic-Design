a4c757c5f9d02e06163ed049250232a71dfa97eb
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-04 11:45:03 +0900

    feat: 숫자 테스트코드 케이스 추가

--- components/molecules/commons/CounterForm/CounterForm.test.tsx
+++ components/molecules/commons/CounterForm/CounterForm.test.tsx
@@ -20,4 +20,26 @@ describe("분자/일반/숫자Form", () => {
     const counter = await screen.findByText(/3/);
     expect(counter).toBeInTheDocument();
   });
+  test("숫자를 누를때 정상적으로 수가 반영되는지 테스트(플러스와 마이너스)", async () => {
+    render(
+      <CounterForm/>
+    );
+    const plusButton = screen.getByRole('plus-button');
+    const minusButton = screen.getByRole('minus-button');
+    user.click(plusButton)
+    .then(() => {
+      user.click(plusButton)
+    })    
+    .then(() => {
+      user.click(plusButton)
+    })
+    .then(() => {
+      user.click(minusButton)
+    })
+    .then(() => {
+      user.click(minusButton)
+    });
+    const counter = await screen.findByText(/1/);
+    expect(counter).toBeInTheDocument();
+  });
 });
